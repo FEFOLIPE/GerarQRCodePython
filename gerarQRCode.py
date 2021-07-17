fd05414ba69ddbdf79fc33a0e2fdf0836f819514
FEFOLIPE <felipe.mamone.vieira@hotmail.com>
2021-07-17 20:35:28 -0300

    First Commit

---
+++ gerarQRCode.py
@@ -0,0 +1,12 @@
+import pyqrcode
+import png
+from pyqrcode import QRCode
+
+# Link para gerar o QRCode
+link = "https://github.com/FEFOLIPE/"
+
+# Gerando o QRCode
+url = pyqrcode.create(link)
+
+# Salvando a imagem
+url.png(r'instagramfefo.png', scale=8)
